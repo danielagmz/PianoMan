c0b8f432fefe884ea9999932c87e92d997be1bbf
Christian270304 <147045888+Christian270304@users.noreply.github.com>
2025-01-11 19:39:42 +0100

    feat: implementar la interacción de las teclas del piano y la reproducción de sonido

---
+++ js/PianoMan.js
@@ -0,0 +1,40 @@
+"use strict";
+///////////////////////////////////////////////////////////
+// Alumnes: Daniela Gamez i Christian Torres
+///////////////////////////////////////////////////////////
+let notas = [['d', 'f', 'g', 'h', 'j', 'a', 's'],
+    ['a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1']];
+function init() {
+    //TouchEmulator();
+    $(document).on('keydown', (event) => {
+        for (let i = 0; i < notas[0].length; i++) {
+            if (event.key === notas[0][i]) {
+                const keyElement = $('#k' + notas[0][i]);
+                keyElement.addClass('activa');
+                setTimeout(() => {
+                    keyElement.removeClass('activa');
+                }, 500);
+                playSound(notas[1][i]);
+            }
+        }
+    });
+    $(document).on('touchstart', (event) => {
+        for (let i = 0; i < notas[0].length; i++) {
+            const target = event.target;
+            if (target && target.id === 'k' + notas[0][i]) {
+                const keyElement = $('#k' + notas[0][i]);
+                keyElement.addClass('activa');
+                setTimeout(() => {
+                    keyElement.removeClass('activa');
+                }, 500);
+                playSound(notas[1][i]);
+            }
+        }
+    });
+}
+function playSound(key) {
+    const sound = new Audio(`assets/${key}.mp3`);
+    sound.play();
+}
+init();
+//# sourceMappingURL=PianoMan.js.map
--- ts/PianoMan.ts
+++ ts/PianoMan.ts
@@ -1,12 +1,45 @@
-"use strict";
+declare const $: any;
 
 ///////////////////////////////////////////////////////////
-// Alumnes: 
+// Alumnes: Daniela Gamez i Christian Torres
 ///////////////////////////////////////////////////////////
+let notas = [['d', 'f', 'g', 'h', 'j', 'a', 's'], 
+			['a1', 'b1', 'c1', 'd1', 'e1', 'f1', 'g1']];
 
 function init() {
-	TouchEmulator();
-
+	//TouchEmulator();
+	$(document).on('keydown',(event:KeyboardEvent) => {
+		for (let i = 0; i < notas[0].length; i++) {
+			if (event.key === notas[0][i]) {
+				const keyElement = $('#k' + notas[0][i]);
+            	keyElement.addClass('activa');
+            	setTimeout(() => {
+                	keyElement.removeClass('activa');
+            	}, 500);
+				playSound(notas[1][i]);
+			}	
+		}
+	});
+	
+	$(document).on('touchstart', (event:TouchEvent) => {
+		for (let i = 0; i < notas[0].length; i++) {
+			const target = event.target as HTMLElement;
+			if (target && target.id === 'k' + notas[0][i]) {
+				const keyElement = $('#k' + notas[0][i]);
+				keyElement.addClass('activa');
+				setTimeout(() => {
+					keyElement.removeClass('activa');
+					}, 500);
+				playSound(notas[1][i]);
+			}
+		}
+	});
+	
 }
+function playSound(key:string) {
+	const sound = new Audio(`assets/${key}.mp3`);
+	sound.play();
+}
+
 
 init();
